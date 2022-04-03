11656ddffef0ff97ca64d8d3a4f2071cf314faf1
Sergio Hidalgo <shidalgoc@auna.pe>
2022-04-03 06:01:32 -0500

    docker, apps en contenedores y message broker con rabbitmq

--- 01-comunicación/frontend/app.js
+++ 01-comunicación/frontend/app.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use("/", express.static(__dirname + "/public"));
 app.use("/api", require("./routes"));
 
-app.get("/health", (req, res) => res.send("I am alive!"));
+app.get("/health", (req, res) => res.send("Todo está funcionando ok"));
 
 app.get("*", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
